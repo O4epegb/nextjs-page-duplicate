990918057313a1f3ad260a74c2bed2e4d4672209
Daniil Demidovich <daniil.demidovich@gmail.com>
2023-07-06 12:41:26 +0200

    Init with example

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,13 +1,13 @@
 import Link from 'next/link'
 
-export default function Home() {
+export default function Test() {
   const number = Math.floor(Math.random() * 100)
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24 gap-2">
       <div>
-        <Link className="underline" href="/">
-          Link to index page
+        <Link className="underline" href="/test">
+          Link to /test page
         </Link>
       </div>
       <div>
